293ac76441a5911e3c03ae4585594a4e30eaa2c5
GedMarc <ged_marc@hotmail.com>
2018-04-22 15:47:19 +0200

    Prepping for RC1 - Update Base Package Name to new Name

--- test/com/jwebmp/plugins/jqueryverticaltimeline/VerticalTimelineTest.java
+++ test/com/jwebmp/plugins/jqueryverticaltimeline/VerticalTimelineTest.java
@@ -1,13 +1,12 @@
 package com.jwebmp.plugins.jqueryverticaltimeline;
 
-import com.jwebmp.BaseTestClass;
 import org.junit.jupiter.api.Test;
 import za.co.mmagon.logger.LogFactory;
 
 import java.util.logging.Logger;
 
 public class VerticalTimelineTest
-		extends BaseTestClass
+
 {
 	Logger log = LogFactory.getLog("Test");
 
