6af5f8865ffdb3733b85c1b525a94cf324d9cb35
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:40 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,6 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
-module com.jwebmp.plugins.jqueryverticaltimeline{
+module com.jwebmp.plugins.jqueryverticaltimeline {
 
 	exports com.jwebmp.plugins.jqueryverticaltimeline;
 
@@ -10,7 +12,12 @@ module com.jwebmp.plugins.jqueryverticaltimeline{
 
 	requires java.validation;
 	requires java.logging;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with com.jwebmp.plugins.jqueryverticaltimeline.VerticalTimelinePageConfigurator;
-	opens com.jwebmp.plugins.jqueryverticaltimeline to com.fasterxml.jackson.databind,com.jwebmp.core;
+
+	provides IGuiceScanJarExclusions with com.jwebmp.plugins.jqueryverticaltimeline.implementations.JQueryVerticalTimelineExclusionsModule;
+	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.jqueryverticaltimeline.implementations.JQueryVerticalTimelineExclusionsModule;
+
+	opens com.jwebmp.plugins.jqueryverticaltimeline to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
