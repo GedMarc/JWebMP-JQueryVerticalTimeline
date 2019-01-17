2e0194109525c0a9523d98e6fc25fd19d2169780
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:30 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/jqueryverticaltimeline/VerticalTimelineParentOptions.java
+++ src/main/java/com/jwebmp/plugins/jqueryverticaltimeline/VerticalTimelineParentOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Marc Magon
+ * @author GedMarc
  * @since 07 Aug 2017
  */
 public class VerticalTimelineParentOptions
