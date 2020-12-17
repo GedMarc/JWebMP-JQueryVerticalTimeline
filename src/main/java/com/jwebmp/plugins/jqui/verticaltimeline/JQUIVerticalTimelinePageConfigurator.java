49c37a60a94ae98ec80ade692004af9fbcf1c4e0
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:02:25 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/jqui/verticaltimeline/JQUIVerticalTimelinePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/jqui/verticaltimeline/JQUIVerticalTimelinePageConfigurator.java
@@ -82,7 +82,7 @@ public class JQUIVerticalTimelinePageConfigurator
 	@NotNull
 	@Override
 	@SuppressWarnings("unchecked")
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
